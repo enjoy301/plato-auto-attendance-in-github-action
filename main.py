410ea55acd226290936c555f553f598a2e774ec6
Eunjong Kim <eunjong147@pusan.ac.kr>
2022-03-16 17:08:42 +0900

    feat: github api 연습

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import time
+from github import Github
+import yaml
 
 
-time.sleep(300)
+with open("secrets.yml", "r") as f:
+    config = yaml.load(f, yaml.BaseLoader)
+    github_token = config['github-token']
+
+repo = Github(github_token).get_repo("enjoy301/plato-auto-attendance-in-github-action")
+
+#repo.create_issue(title="This is a new issue", body="This is the issue body")
+opened_issues = repo.get_issues(state='open')
+for issue in opened_issues:
+    print(issue.number)
